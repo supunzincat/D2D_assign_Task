22aa4a4fe962cb04390fd0b1099000d36c551924
Supun_zincat <Supun_zincat@05068-020L.dialog.dialoggsm.com>
2018-12-17 17:27:28 +0530

    Added is enabled

--- Dor2Dor.test/Dor2dor/pages/ViewTaskPage.java
+++ Dor2Dor.test/Dor2dor/pages/ViewTaskPage.java
@@ -1,6 +1,15 @@
 package pages;
 
+import static org.testng.Assert.assertNull;
+import static org.testng.Assert.assertTrue;
+
+import java.awt.Button;
+import java.security.PublicKey;
+
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
 public class ViewTaskPage {
@@ -12,10 +21,109 @@ public class ViewTaskPage {
 		this.d=d;
 	}
 
+	@FindBy(how=How.XPATH,using="//*[@id='districts_filter_rightbar']") WebElement DDDistrict;
+	@FindBy(how=How.XPATH,using="//*[@id='territory_filter_rightbar']") WebElement DDTerriotorry;
+	@FindBy(how=How.XPATH,using="//*[@id='gn_divisions_on_filter_form']") WebElement DDGNDivisions;
+	
+	@FindBy(how=How.XPATH,using="//*[@id='sdate']") WebElement DDStartdate;
+	
+	@FindBy(how=How.XPATH,using="//*[@id='edate']") WebElement DDEnddate;
+	
+	@FindBy(how=How.XPATH,using="//*[@id='d2d-tassign-rightbar-wrap']/form/div[6]/label/span") WebElement CheckBoxTowers;
+	@FindBy(how=How.XPATH,using="//*[@id='js-filter_map_towers']") WebElement TowersDropdown;
+	
+	@FindBy(how=How.XPATH,using="//*[@id='d2d-tassign-rightbar-wrap']/form/div[6]/div/div") WebElement DDTowers;
+	
+	@FindBy(how=How.XPATH,using="//*[@id='remarks']") WebElement TextBoxRemarksTex;
 	
 	
-	public void DropDownisenable() {
+	@FindBy(how=How.XPATH,using="//*[@id='d2d-tassign-rightbar-wrap']/form/div[8]/div/div/div/div/button[3]") WebElement EditButton;
+	
+	
+	
+	public void DropDownisenable(WebElement element) {
 		// TODO Auto-generated method stub
+		//boolean isDisplayed= classes.contains("btn dropdown-toggle disabled");
+	
+//element.isEnabled();
+	}
+	
+	
+	public void VeriffDDNotEnabled(WebElement DDElement) {
+		// TODO Auto-generated method stub
+		//d.get("http://wom/v3staging/d2d/SingleTask/manage/4");
+		String isdissable= DDElement.getAttribute("disabled");
+		boolean isDisplayed1= isdissable.contains("true");
+		assertTrue(isDisplayed1);
 
 	}
+	
+	public void VeriffDDEnabled(WebElement DDElement) {
+		// TODO Auto-generated method stub
+		//d.get("http://wom/v3staging/d2d/SingleTask/manage/4");
+		String isdissable= DDElement.getAttribute("disabled");
+		//boolean isDisplayed1= isdissable.contains(null);
+		assertNull(isdissable);
+
+	}
+	
+	public void verifycheckboxisenable(WebElement DDElement) {
+		// TODO Auto-generated method stub
+DDElement.isEnabled();
+	}
+		
+	public void VerifyDistrictISEnabled() {
+		// TODO Auto-generated method stub
+		VeriffDDNotEnabled(DDDistrict);
+	}
+	
+	public void TerriotorryISEnabled() {
+		// TODO Auto-generated method stub
+		VeriffDDNotEnabled(DDTerriotorry);
+	}
+	
+	public void VerifyDDGNDivisionsISEnabled() {
+		// TODO Auto-generated method stub
+		VeriffDDNotEnabled(DDGNDivisions);
+	}
+	
+	public void VerifyDDStartdateISEnabled() {
+		// TODO Auto-generated method stub
+		VeriffDDNotEnabled(DDStartdate);
+	}
+	
+	public void VerifyDDEnddateISEnabled() {
+		// TODO Auto-generated method stub
+		VeriffDDNotEnabled(DDEnddate);
+	}
+	public void VerifyDDTowersISEnabled() {
+		// TODO Auto-generated method stub
+		verifycheckboxisenable(CheckBoxTowers);
+	}
+	
+	public void VerifyDDTowersDropdownISEnabled() {
+		// TODO Auto-generated method stub
+		verifycheckboxisenable(TowersDropdown);
+	}
+	
+	public void VerifyTextBoxRemarksTexNotEnabled() {
+		// TODO Auto-generated method stub
+		VeriffDDNotEnabled(TextBoxRemarksTex);
+	}
+	
+	public void ClickOnButtons(WebElement ButtonName) {
+		// TODO Auto-generated method stub
+ButtonName.click();
+	}
+	
+	public void ClickOneditButton() {
+		// TODO Auto-generated method stub
+		ClickOnButtons(EditButton);
+	}
+	
+	public void VerifyTextBoxRemarksTexISEnabled() {
+		// TODO Auto-generated method stub
+		VeriffDDEnabled(TextBoxRemarksTex);
+	}
+	
 }
--- Dor2Dor.test/Dor2dor/testCases/ViewSpecificTask.java
+++ Dor2Dor.test/Dor2dor/testCases/ViewSpecificTask.java
@@ -1,5 +1,7 @@
 package testCases;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
@@ -11,12 +13,89 @@ public class ViewSpecificTask {
 
 	public class TesClass {
 		
-		WebDriver d=LaunchWebDriver.WebdriverLaunch();
+		WebDriver driver= LaunchWebDriver.WebdriverLaunch();
 		
 		@Test(priority=1)
 		public void TC001_verify_District_Is_enable() {
-	ViewTaskPage viewtask= new ViewTaskPage(d);
+			LoginPage Login= new LoginPage(driver);
+			Login.LoginToWom();			
+		
 		}
 		
+		
+		
+		@Test(priority=2)
+		public void TC002_verify_District_Is_enable() {
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			driver.get("http://wom/v3staging/d2d/SingleTask/manage/4");
+			ViewTaskPage viewtask= new ViewTaskPage(driver);
+			viewtask.VerifyDistrictISEnabled();
+			
+		}
+		
+		@Test(priority=3)
+		public void TC003_verify_VerifyDDTerriotorryISEnabled_Is_enable() {
+			driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+			ViewTaskPage viewtask= new ViewTaskPage(driver);
+			viewtask.TerriotorryISEnabled();
+			
+		}
+		
+		@Test(priority=4)
+		public void TC004_verify_VerifyDDGNDivisionsISEnabled_Is_enable() {
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			ViewTaskPage viewtask= new ViewTaskPage(driver);
+			viewtask.VerifyDDGNDivisionsISEnabled();
+			
+		}
+		
+		@Test(priority=5)
+		public void TC005_verify_VerifyDDStartdateISEnabled_Is_enable() {
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			ViewTaskPage viewtask= new ViewTaskPage(driver);
+			viewtask.VerifyDDStartdateISEnabled();
+			
+		}
+		
+		@Test(priority=6)
+		public void TC006_verify_VerifyDDEnddateISEnabled_Is_enable() {
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			ViewTaskPage viewtask= new ViewTaskPage(driver);
+			viewtask.VerifyDDEnddateISEnabled();
+			
+		}
+		
+		@Test(priority=7)
+		public void TC007_VerifyTowersCheckboxISEnabled_Is_enable() {
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			ViewTaskPage viewtask= new ViewTaskPage(driver);
+			viewtask.VerifyDDTowersISEnabled();
+			
+		}
+		
+		@Test(priority=8)
+		public void TC008_VerifyDDTowersISEnabled_Is_enable() {
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			ViewTaskPage viewtask= new ViewTaskPage(driver);
+			viewtask.VerifyDDTowersDropdownISEnabled();
+			
+		}
+		
+		@Test(priority=9)
+		public void TC009_VerifyTextBoxRemarksTexISEnabled_Is_enable() {
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			ViewTaskPage viewtask= new ViewTaskPage(driver);
+			viewtask.VerifyTextBoxRemarksTexNotEnabled();
+			
+		}
+		
+		@Test(priority=10)
+		public void TC010_VerifyTextBoxRemarksTexISEnabled_Is_enableAfterClick() {
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+			ViewTaskPage viewtask= new ViewTaskPage(driver);
+			viewtask.ClickOneditButton();
+			viewtask.VerifyTextBoxRemarksTexISEnabled();
+			
+		}
 }
 	}
