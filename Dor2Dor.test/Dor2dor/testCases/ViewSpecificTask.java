604ca8f206ca174b23f5f2654d41f149441c6d18
Supun_zincat <Supun_zincat@05068-020L.dialog.dialoggsm.com>
2018-12-20 13:26:10 +0530

    TC17

--- Dor2Dor.test/Dor2dor/testCases/ViewSpecificTask.java
+++ Dor2Dor.test/Dor2dor/testCases/ViewSpecificTask.java
@@ -156,7 +156,7 @@ public class ViewSpecificTask {
 			
 		}
 		@Test(priority=18)
-		public void TC010_VerifyTextBoxRemarksTexISEnabled_Is_enableAftertest() {
+		public void TC010_VerifyTextBoxRemarksTexISEnabled_Is_enableAftertest1() {
 			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			ViewTaskPage viewtask= new ViewTaskPage(driver);
 			viewtask.VerifyTextBoxRemarksTexISEnabled();
